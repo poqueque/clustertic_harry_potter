cab0aea19430eac2156d7f0345b0ffde3a486b1d
Edi <poqueque@yahoo.es>
2023-11-14 19:12:31 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:harry_potter/screens/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        fontFamily: GoogleFonts.montserrat().fontFamily,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
